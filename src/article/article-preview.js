b34deba1b3c50996c79b866481504fed6924ad16
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2017-07-13 14:03:45 +0530

    refactored article body into array

--- src/article/article-preview.js
+++ src/article/article-preview.js
@@ -2,18 +2,12 @@ import React from 'react';
 
 export default class ArticlePreview extends React.Component{
 
-  constructor(){
-    super();
-
-    //alert(Style.label);
-  }
-
   render(){
     return(
       <div className="panel panel-primary">
         <div className="panel-heading"><h1>{this.props.title}</h1></div>
-        <div className="panel-body"><p>{this.props.body}</p></div>
-        <div className="panel-footer"><label>{this.props.authorName}</label></div>
+        <div className="panel-body"><p>{this.props.body[0]}</p></div>
+        <div className="panel-footer">written by - <label>{this.props.authorName}</label></div>
       </div>
     );
   }
