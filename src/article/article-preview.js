77c70ff79885f9a9d4ccca6c3389fc7a3ff59660
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2017-07-13 22:34:55 +0530

    completed article page and added styles

--- src/article/article-preview.js
+++ src/article/article-preview.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export default class ArticlePreview extends React.Component{
 
   render(){
 
     return(
-      <div className="panel panel-primary">
-        <div className="panel-heading"><h1>{this.props.title}</h1></div>
+      <Link to={"/article/"+this.props.id}>
+      <div className="panel panel-info">
+        <div className="panel-heading text-center"><h1>{this.props.title}</h1></div>
         <div className="panel-body"><p>{this.props.body[0].content}</p></div>
-        <div className="panel-footer">written by - <label>{this.props.authorName}</label></div>
+        <div className="panel-footer">written by - <label>{this.props.author.name}</label></div>
       </div>
+      </Link>
 
     );
   }
--- src/article/article.js
+++ src/article/article.js
@@ -18,7 +18,12 @@ export default class Article extends React.Component{
             content:"this is a tesst"
           }
       ],
-        authorName:"tester"
+        author:{
+          name:"tester",
+          bio:"i am a tester",
+          image:"",
+          site:""
+        }
       }
     }
   }
@@ -38,14 +43,28 @@ export default class Article extends React.Component{
   }
 
   render(){
-    let _articleJSX = this.state.article.body.map(b=><div key={b.paraNumber}>
-                                                  <h2 key={`heading ${b.paraNumber}`}>{b.heading}</h2>
-                                                  <p key={`para ${b.paraNumber}`}>{b.content}</p>
-                                                </div>);
+    let _articleJSX = this.state.article.body.map(b=><div>
+                                                      <h2 key={`heading ${b.paraNumber}`}>{b.heading}</h2>
+                                                      <p className="text-justify" key={`para ${b.paraNumber}`}>{b.content}</p>
+                                                    </div>);
+    const _authorJSX = <div className="col-md-offset-1 col-md-3">
+                        <h3>About the author</h3>
+                        <div className="thumbnail">
+                          <img src={this.state.article.author.image} alt={this.state.article.author.name} />
+                          <div className="caption">
+                            {this.state.article.author.bio}
+                          </div>
+                        </div>
+                      </div>;
 
     return <div>
-            <h1>{this.state.article.title}</h1>
-            {_articleJSX}
+            <h1 className="page-header">{this.state.article.title}</h1>
+            <div className="col-md-4">written by - <strong>{this.state.article.author.name}</strong></div>
+            <div className="col-md-4">written on - {this.state.article.writtenOn}</div>
+            <div className="col-md-8">
+              {_articleJSX}
+            </div>
+            {_authorJSX}
           </div>
   }
 }
