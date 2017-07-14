4afaac79e994108f2dac86d1241df7deb7d8c4bf
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2017-07-14 23:49:04 +0530

    added a listener to submit in comment-form

--- src/comments/comment-form.js
+++ src/comments/comment-form.js
@@ -3,10 +3,32 @@ import React from 'react';
 export default class CommentForm extends React.Component{
 
   render(){
-    return(<form>
-            <input type="text" placeholder="name" />
-            <textarea placeholder="name"></textarea>
-            <input type="submit" value="SUBMIT" />
-          </form>);
+
+    const _formJSX = <form className="well-form">
+                      <div className="form-group">
+                        <label>name</label>
+                        <input className="form-control" type="text" placeholder="name" ref={input=>this.name = input}/>
+                      </div>
+                      <div className="form-group">
+                        <label>comment</label>
+                        <textarea className="form-control"
+                                  placeholder="enter your comment here"
+                                  ref={input=>this.comment = input}>
+                        </textarea>
+                      </div>
+                    <input type="submit" className="btn btn-success" value="SUBMIT" onClick={this._formSubmitted.bind(this)}/>
+                  </form>;
+    return(<div>
+            {_formJSX}
+          </div>);
+  }
+
+  _formSubmitted(event){
+    event.preventDefault();
+
+    alert(this.name.value);
+    this.name.value = "";
+    alert(this.comment.value);
+    this.comment.value = "";
   }
 }
